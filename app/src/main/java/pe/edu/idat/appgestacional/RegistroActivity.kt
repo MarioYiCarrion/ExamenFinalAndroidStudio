2703ef3820d5c00c4343d205cb5bf2a2c7365fc9
MarioYiCarrion <marioyicarrion@hotmail.com>
2024-03-25 18:25:01 -0500

    Modificaciones en el RegistroActivity

--- app/src/main/java/pe/edu/idat/appgestacional/RegistroActivity.kt
+++ app/src/main/java/pe/edu/idat/appgestacional/RegistroActivity.kt
@@ -1,5 +1,6 @@
 package pe.edu.idat.appgestacional
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -9,9 +10,9 @@ import android.widget.Toast
 class RegistroActivity : AppCompatActivity() {
 
     private lateinit var editTextName: EditText
-    private  lateinit var editTextDNI: EditText
-    private  lateinit var editTextDireccion: EditText
-    private  lateinit var editTextCelular: EditText
+    private lateinit var editTextDNI: EditText
+    private lateinit var editTextDireccion: EditText
+    private lateinit var editTextCelular: EditText
     private lateinit var editTextEmail: EditText
     private lateinit var editTextUsername: EditText
     private lateinit var editTextPassword: EditText
@@ -20,6 +21,7 @@ class RegistroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_registro)
 
         // Inicialización de vistas
@@ -41,14 +43,17 @@ class RegistroActivity : AppCompatActivity() {
 
     private fun registerUser() {
         val name = editTextName.text.toString().trim()
+        val dni = editTextDNI.text.toString().trim()
+        val direccion = editTextDireccion.text.toString().trim()
+        val celular = editTextCelular.text.toString().trim()
         val email = editTextEmail.text.toString().trim()
         val username = editTextUsername.text.toString().trim()
         val password = editTextPassword.text.toString().trim()
         val confirmPassword = editTextConfirmPassword.text.toString().trim()
 
         // Validación de campos vacíos
-        if (name.isEmpty() || email.isEmpty() || username.isEmpty() || password.isEmpty() ||
-            confirmPassword.isEmpty()) {
+        if (name.isEmpty() || dni.isEmpty() || direccion.isEmpty() || celular.isEmpty() || email.isEmpty() ||
+            username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
             // Mostrar mensaje de error si algún campo está vacío
             showToast("Por favor, completa todos los campos.")
             return
@@ -63,10 +68,11 @@ class RegistroActivity : AppCompatActivity() {
 
         // Mostrar mensaje de éxito si todos los campos son válidos
         showToast("Usuario registrado correctamente.")
+        // Redirigir al usuario al MainActivity
+        startActivity(Intent(this, MainActivity::class.java))
     }
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
-
