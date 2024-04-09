fc92ab82351474a582e90c1c94b086002a459a2f
MarioYiCarrion <marioyicarrion@hotmail.com>
2024-04-08 23:07:56 -0500

    Conexion a apis

--- app/src/main/java/pe/edu/idat/appgestacional/util/bdclases/Seguimiento.kt
+++ app/src/main/java/pe/edu/idat/appgestacional/util/bdclases/Seguimiento.kt
@@ -1,10 +1,11 @@
 package pe.edu.idat.appgestacional.util.bdclases
 
 data class Seguimiento(
-    val fechaCita:   String ="",
+    val fechaCita: String = "",
     val ultimaRegla: String = "",
     val fpp: String = "",
     val semanaGestacion: String = "",
     val citaMedica: String = "",
-    val userId: String = ""
+    val userId: String = "",
+    val userId1: String
 )
