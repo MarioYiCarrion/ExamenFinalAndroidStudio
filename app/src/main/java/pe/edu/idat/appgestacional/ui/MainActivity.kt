19e42ac933817bcbfff20885e827d2a8730b185c
MarioYiCarrion <marioyicarrion@hotmail.com>
2024-04-11 21:28:45 -0500

    correo en el bar

--- app/src/main/java/pe/edu/idat/appgestacional/ui/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/appgestacional/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -14,6 +15,8 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import pe.edu.idat.appgestacional.R
 import pe.edu.idat.appgestacional.databinding.ActivityMainBinding
 
@@ -21,24 +24,46 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var txtusuario: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Inflar el diseño de la actividad principal
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configurar la barra de herramientas
         setSupportActionBar(binding.appBarMain.toolbar)
 
+        // Configurar el botón flotante
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        // Inicializar Firebase Authentication y DatabaseReference
+        auth = FirebaseAuth.getInstance()
+        databaseReference = FirebaseDatabase.getInstance().reference.child("User")
+
+        // Encontrar el TextView txtusuario en el encabezado de navegación
+        val headerView = binding.navView.getHeaderView(0)
+        txtusuario = headerView.findViewById(R.id.txtusuario)
+
+        // Obtener el usuario actualmente autenticado y mostrar su correo electrónico en txtusuario
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val email = currentUser.email
+            txtusuario.text = email
+        }
+
+        // Configurar la navegación con el controlador de navegación y el DrawerLayout
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.principalFragment, R.id.seguimientoFragment, R.id.citasFragment,
@@ -46,12 +71,13 @@ class MainActivity : AppCompatActivity() {
                 R.id.recordatoriosyAlertasFragment
             ), drawerLayout
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflar el menú de opciones
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
@@ -60,11 +86,8 @@ class MainActivity : AppCompatActivity() {
         // Manejar los clics en los elementos del menú
         when (item.itemId) {
             R.id.Logout -> {
-                // Aquí puedes agregar el código para cerrar la sesión
-                // Por ejemplo, iniciar la actividad de inicio de sesión o limpiar la sesión actual
-                // Si estás utilizando Firebase Auth, puedes hacer algo como:
+                // Cerrar sesión y redirigir al usuario a la actividad de inicio de sesión
                 FirebaseAuth.getInstance().signOut()
-                // Y luego redirigir al usuario a la actividad de inicio de sesión, por ejemplo:
                 val intent = Intent(this, LoginActivity::class.java)
                 startActivity(intent)
                 finish() // Cierra la actividad actual
@@ -75,6 +98,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
+        // Navegar hacia arriba en la jerarquía de la navegación
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
